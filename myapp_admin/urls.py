8957013eb803035c86b445e48a5f6730a73e53c4
sneham10 <sneha.muralidharan10@gamil.com>
2022-04-01 10:02:33 +0530

    done

--- myapp_admin/urls.py
+++ myapp_admin/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mydapp import views
+from myapp_app import views
 urlpatterns = [
+path('admin/', admin.site.urls),
     path('index/', views.index),
 ] 
