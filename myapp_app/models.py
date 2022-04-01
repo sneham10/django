3802fbe1c841e4edc799349751b2fa91a95a438d
sneham10 <sneha.muralidharan10@gamil.com>
2022-04-01 10:15:43 +0530

    done

--- myapp_app/models.py
+++ myapp_app/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django.forms import CharField
-# Create your models here.
 
-class Employee 
+
+class Employee(models.Model):    
+    first_name = models.CharField(max_length=100, default='')
+    last_name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(max_length=200, blank=True)
+
+    class Meta:
+        db_table = "employee"
--- myapp_app/views.py
+++ myapp_app/views.py
@@ -1,23 +1,17 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
-import datetime as dt
-import datetime
-from pytz import timezone
-import pytz
-def index(req):
+from django.shortcuts import redirect, render
 
-    blr = datetime.datetime.now().time()
-    nyc = datetime.datetime.now(pytz.timezone('US/Eastern'))
-    singapore = datetime.datetime.now(pytz.timezone('Asia/Singapore'))
-    sydney = datetime.datetime.now(pytz.timezone('Australia/Sydney'))
-    x={
-        "t_blr": "%s"%blr,
-        "t_nyc": "%s"%nyc,
-        "t_sing": "%s"%singapore,
-        "t_sy": "%s"%sydney,
-   
-      } 
-    tmpl=loader.get_template('index.html')
-    return HttpResponse(tmpl.render(x))
-  
+from myapp_app.form import EmployeeForm
+
+
+def index(req):
+    if req.method == 'POST':
+        empForm = EmployeeForm(req.POST)
+        if empForm.is_valid():
+            return HttpResponse("good job")
+        else:
+            return render(req, 'index.html', {'form': empForm})
+    else:
+        emp = EmployeeForm()
+        return render(req, "index.html", {"form": emp})
+
